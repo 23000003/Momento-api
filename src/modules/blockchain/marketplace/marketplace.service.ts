d46843cc2d28562aafc9a0c529d6562be11fb97e
23000003 <kurtdicdican@gmail.com>
2025-02-09 13:32:41 +0800

    init: marketplace services

--- src/modules/blockchain/marketplace/marketplace.service.ts
+++ src/modules/blockchain/marketplace/marketplace.service.ts
@@ -4,4 +4,45 @@ import { BlockchainConfigService } from "../blockchain-config.service";
 @Injectable()
 export class MarketplaceService {
   constructor(private readonly web3: BlockchainConfigService) {}
+
+  getCollectionData() {
+    return {
+      uniqueHolders: 0,
+      totalSupply: 0,
+      floorPrice: 0,
+      totalVolume: 0,
+      onSale: {
+        metadata: [],
+        price: 0,
+      },
+    };
+  }
+
+  getNFTData() {
+    return {
+      metadata: [],
+      price: 0,
+    };
+  }
+
+  sellNFT() {
+    return {
+      status: "success",
+      message: "NFT listed successfully",
+    };
+  }
+
+  buyNFT() {
+    return {
+      status: "success",
+      message: "NFT bought successfully",
+    };
+  }
+
+  delistNFT() {
+    return {
+      status: "success",
+      message: "NFT delisted successfully",
+    };
+  }
 }
